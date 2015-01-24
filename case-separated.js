9415c78f85108829a814087ee7a78a26704773fc
Malte-Maurice Dreyer <dreyer.maltem@gmail.com>
2015-01-24 21:08:21 +0000

    Fix methods not using separator property

--- case-separated.js
+++ case-separated.js
@@ -1,28 +1,26 @@
 function CaseSeparated(separator) {
-    Object.defineProperty(this, {
-        separator: {
-            value: separator,
-            enumerable: true,
-            configurable: false
-        }
+    Object.defineProperty(this, "separator", {
+        value: separator,
+        enumerable: true,
+        configurable: false
     });
 }
 
-CaseSeparated.prototype.parse = function(val, separator) {
+CaseSeparated.prototype.parse = function(val) {
     if(typeof(val) === "undefined" || val === null) {
         return [];
     }
 
-    return String(val).split(separator);
+    return String(val).split(this.separator);
 };
 
-CaseSeparated.prototype.stringify = function(val, separator) {
+CaseSeparated.prototype.stringify = function(val) {
     if(typeof(val) === "undefined" || val === null) {
         return "";
     }
 
     if(Array.isArray(val)) {
-        return val.map(this.stringify).join(separator);
+        return val.map(this.stringify).join(this.separator);
     }
 
     return String(val);
