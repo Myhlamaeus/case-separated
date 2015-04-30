f6717c4bcb3da062dc5dba7da44125e18e5f1fb6
Malte-Maurice Dreyer <dreyer.maltem+dev@gmail.com>
2015-04-30 13:12:04 +0000

    Use ES6 everywhere & replace grunt with gulp

--- case-separated.js
+++ case-separated.js
@@ -1,33 +1,33 @@
-function CaseSeparated (separator) {
-  Object.defineProperty(this, 'separator', {
-    value: separator,
-    enumerable: true,
-    configurable: false
-  })
-}
-
-CaseSeparated.prototype.parse = function (val) {
-  if (typeof (val) === 'undefined' || val === null) {
-    return []
+export default class CaseSeparated {
+  constructor (separator) {
+    Object.defineProperty(this, 'separator', {
+      value: separator,
+      enumerable: true,
+      configurable: false
+    })
   }
 
-  return String(val).split(this.separator)
-}
+  parse (val) {
+    if (typeof (val) === 'undefined' || val === null) {
+      return []
+    }
 
-CaseSeparated.prototype.stringify = function (val) {
-  if (typeof (val) === 'undefined' || val === null) {
-    return ''
+    return String(val).split(this.separator)
   }
 
-  if (Array.isArray(val)) {
-    return val.map(this.stringify).join(this.separator)
+  stringify (val) {
+    if (typeof (val) === 'undefined' || val === null) {
+      return ''
+    }
+
+    if (Array.isArray(val)) {
+      return val.map(this.stringify).join(this.separator)
+    }
+
+    return String(val)
   }
 
-  return String(val)
+  is (val) {
+    return typeof (val) !== 'undefined' && val !== null && String(val).indexOf(this.separator) !== -1
+  }
 }
-
-CaseSeparated.prototype.is = function (val) {
-  return typeof (val) !== 'undefined' && val !== null && String(val).indexOf(this.separator) !== -1
-}
-
-module.exports = CaseSeparated
--- test/case-separated.js
+++ test/case-separated.js
@@ -1,11 +1,11 @@
 /* global describe, it */
 
-var CaseSeparated = require('../case-separated'),
-  assert = require('assert')
+import CaseSeparated from '../case-separated'
+import assert from 'assert'
 
 describe('case-separated', function () {
-  var separators = ['.', '_', '-', 'whatever'],
-    tests = [
+  const separators = ['.', '_', '-', 'whatever']
+  const tests = [
       ['T'],
       ['t'],
       ['Test'],
@@ -16,43 +16,43 @@ describe('case-separated', function () {
   ]
 
   describe('#parse()', function () {
-    separators.forEach(function (separator) {
-      var caseSeparated = new CaseSeparated(separator)
+    for (let separator of separators) {
+      const caseSeparated = new CaseSeparated(separator)
 
-      tests.forEach(function (test) {
-        var name = test.join(separator),
-          parsed = test
+      for (let test of tests) {
+        const name = test.join(separator)
+        const parsed = test
 
         it(JSON.stringify(name) + ' -> ' + JSON.stringify(parsed), function () {
           assert.deepEqual(caseSeparated.parse(name), parsed)
         })
-      })
-    })
+      }
+    }
   })
 
   describe('#stringify()', function () {
-    separators.forEach(function (separator) {
-      var caseSeparated = new CaseSeparated(separator)
+    for (let separator of separators) {
+      const caseSeparated = new CaseSeparated(separator)
 
-      tests.forEach(function (test) {
-        var name = test.join(separator),
-          parsed = test
+      for (let test of tests) {
+        const name = test.join(separator)
+        const parsed = test
 
         it(JSON.stringify(parsed) + ' -> ' + JSON.stringify(name), function () {
           assert.equal(caseSeparated.stringify(parsed), name)
         })
-      })
-    })
+      }
+    }
   })
 
   describe('#is()', function () {
-    separators.forEach(function (separator) {
-      var caseSeparated = new CaseSeparated(separator)
+    for (let separator of separators) {
+      const caseSeparated = new CaseSeparated(separator)
 
-      tests.forEach(function (test) {
-        var name1 = test.join(separator),
-          name2 = test.join(separator === separators[0] ? separators[1] : separators[0]),
-          val1 = test.length > 1
+      for (let test of tests) {
+        const name1 = test.join(separator)
+        const name2 = test.join(separator === separators[0] ? separators[1] : separators[0])
+        const val1 = test.length > 1
 
         it(JSON.stringify(name1) + ' (' + val1 + ')', function () {
           assert.equal(caseSeparated.is(name1), val1)
@@ -60,7 +60,7 @@ describe('case-separated', function () {
         it(JSON.stringify(name2) + ' (false)', function () {
           assert.equal(caseSeparated.is(name2), false)
         })
-      })
-    })
+      }
+    }
   })
 })
